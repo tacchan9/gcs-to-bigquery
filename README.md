bfe2ec3295eb9710bf0d4ec9dca065335e8d0b24
hiroaki_tateishi@densan-s.co.jp <hiroaki_tateishi@densan-s.co.jp>
2019-03-05 15:29:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# GCS To Big Query Cloud Function
+Google Cloud Function for processing `json` log files in Google Cloud Storage and creating BigQuery tables.
+
+## Deploy from gcloud CLI
+#### Open Google Cloud Shell
+```bash
+curl -LO "https://github.com/tacchan9/gcs-to-bigquery/archive/master.zip" && unzip master.zip && cd gcs-to-bigquery-master
+```
+#### Update the environment variables in deploy.sh (e.g. `nano deploy.sh`)
+```bash
+
+BUCKET_NAME="" # required – The name of Google Cloud Storage bucket used for Cloudflare Logpush logs.
+DATASET="" # optional – BigQuery dataset to write to. Will be created if necessary.
+TABLE="" # optional – BigQuery table to write to. Will be created if necessary.
+```
+#### Deploy function
+```bash
+sh ./deploy.sh
+```
+
+  
+*Note: You can also deploy this function locally using the same steps below. The only requirement is the [gcloud SDK](https://cloud.google.com/sdk/downloads).*
---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const { BigQuery } = require('@google-cloud/bigquery')
+const { Storage } = require('@google-cloud/storage')
+
+async function gcsbq (file, context) {
+  const _schema = require(process.env.SCHEMA)
+
+  const datasetId = process.env.DATASET
+  const tableId = process.env.TABLE
+
+  const bigquery = new BigQuery()
+
+  const storage = new Storage()
+
+  console.log(`Starting job for ${file.name}`)
+
+  const filename = storage.bucket(file.bucket).file(file.name)
+
+  /* Configure the load job and ignore values undefined in schema */
+  const metadata = {
+    sourceFormat: 'CSV',
+    schema: {
+      fields: _schema
+    },
+    ignoreUnknownValues: true
+  }
+
+  const dataset = bigquery.dataset(datasetId)
+
+  await dataset.get({ autoCreate: true }, (e, dataset, res) => {
+    if (e) console.log(e)
+    dataset.table(tableId).get({ autoCreate: true }, (e, table, res) => {
+      table.load(filename, metadata)
+    })
+  })
+}
+
+exports.gcsbq = gcsbq
